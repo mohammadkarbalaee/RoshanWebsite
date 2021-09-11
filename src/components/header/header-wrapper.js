aa1eb5915629acca4867fa5c559f0839da28fb54
Muhammad <muhammad.ksht@gmail.com>
2021-09-11 11:31:41 +0430

    header is finished but is not responsive

--- src/components/header/header-wrapper.js
+++ src/components/header/header-wrapper.js
@@ -10,7 +10,7 @@ class Header extends Component {
         return(
             <div className="header-wrapper">
                <Navbar/>
-                <RoshanEarthBackground/>
+               <RoshanEarthBackground/>
             </div>
         )
     }
--- src/components/header/roshan-earth-background/roshan-earth-background.js
+++ src/components/header/roshan-earth-background/roshan-earth-background.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {Component} from 'react';
 import './roshan-earth-background.scss'
 import background from "../../../assets/images/background.svg";
-import BigTitle from "./big-title/bit-title";
+import roshanBigTitle from "../../../assets/images/roshan-big-title.svg";
+import description from "../../../assets/images/description.svg";
+import mouse from "../../../assets/images/mouse.svg";
 
 class RoshanEarthBackground extends Component {
     render() {
         return(
             <div className='earth-background'>
-                <img src={background} alt={'fail'}/>
-                <BigTitle/>
+                <img className='background' src={background} alt={'fail'}/>
+                <img className='big-title' src={roshanBigTitle} alt={'fail'}/>
+                <img className='description' src={description} alt={'fail'}/>
+                <img className='mouse' src={mouse} alt={'fail'}/>
             </div>
         )
     }

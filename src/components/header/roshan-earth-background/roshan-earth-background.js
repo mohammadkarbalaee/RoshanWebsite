b1a2c1ffa2ae9833dc3dc7b4f0605bc9bb4d9051
Muhammad <muhammad.ksht@gmail.com>
2021-09-13 21:49:58 +0430

    some minor changes added

--- src/components/header/roshan-earth-background/roshan-earth-background.js
+++ src/components/header/roshan-earth-background/roshan-earth-background.js
@@ -5,7 +5,6 @@ import background from "../../../assets/images/background.svg";
 import roshanBigTitle from "../../../assets/images/roshan-big-title.svg";
 import description from "../../../assets/images/description.svg";
 import descriptionMobile from "../../../assets/images/description-mobile.svg";
-import mouse from "../../../assets/images/mouse.svg";
 import backgroundMobile from "../../../assets/images/background-mobile.png";
 
 class RoshanEarthBackground extends Component {

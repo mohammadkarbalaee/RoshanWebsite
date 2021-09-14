55e3746ace58443275a3a8d79239295be6090810
Muhammad <muhammad.ksht@gmail.com>
2021-09-14 18:21:21 +0430

    fixed responsive flawlessly

--- src/components/header/roshan-earth-background/roshan-earth-background.js
+++ src/components/header/roshan-earth-background/roshan-earth-background.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import {Component} from 'react';
 import './roshan-earth-background.scss'
-import background from "../../../assets/images/background.svg";
-import roshanBigTitle from "../../../assets/images/roshan-big-title.svg";
-import description from "../../../assets/images/description.svg";
-import descriptionMobile from "../../../assets/images/description-mobile.svg";
-import backgroundMobile from "../../../assets/images/background-mobile.png";
-import Lottie from 'lottie-react';
-import mouseAnimation from '../../../assets/lotties/mouse-scrolling.json';
+import WebBackground from "../../../assets/images/web-background.svg";
+import MobileBackground from "../../../assets/images/mobile-background.svg";
 
 
 class RoshanEarthBackground extends Component {
     render() {
-        const mouseAnimationStyle = {
-            height: 500,
-            marginTop:600,
-            marginRight:530,
-            display:'flex',
-            position:'absolute',
-        };
-
         return(
             <div className='earth-background'>
-                <Lottie
-                    animationData={mouseAnimation}
-                    style={mouseAnimationStyle}
-                    className={'mouse-animation'}
-                />
-                <img className='background' src={background} alt={'fail'}/>
-                <img className='background-mobile' src={backgroundMobile} alt={'fail'}/>
-                <img className='big-title' src={roshanBigTitle} alt={'fail'}/>
-                <img className='description' src={description} alt={'fail'}/>
-                <img className='description-mobile' src={descriptionMobile} alt={'fail'}/>
+                <img className='web-background' src={WebBackground} alt={'fail'}/>
+                <img className='mobile-background' src={MobileBackground} alt={'fail'}/>
             </div>
         )
     }

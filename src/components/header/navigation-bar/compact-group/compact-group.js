6d84e76ed007e846d98be0c96454b414725d6a1b
Muhammad <muhammad.ksht@gmail.com>
2021-09-14 10:26:55 +0430

    added toggle

--- src/components/header/navigation-bar/compact-group/compact-group.js
+++ src/components/header/navigation-bar/compact-group/compact-group.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react';
 import './compact-group.scss';
 import { ReactComponent as EqualsIcon } from '../../../../assets/images/equals-icon.svg';
+import { ReactComponent as CloseIcon } from '../../../../assets/images/close-icon.svg';
 
 class CompactGroup extends Component {
+
+    state = {
+        isOpen:false,
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    };
+
     render() {
         return(
             <div className='compact-group'>
-                <EqualsIcon className='icon'/>
+                <button onClick={this.toggle}>
+                    <EqualsIcon className='icon' id={this.state.isOpen ? 'hidden' : ''}/>
+                    <CloseIcon className='icon' id={this.state.isOpen ? '' : 'hidden'}/>
+                </button>
             </div>
         )
     }

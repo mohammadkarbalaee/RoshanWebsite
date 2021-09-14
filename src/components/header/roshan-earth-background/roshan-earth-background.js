7071676f9d431eee8ba7f447600e936fbc978011
Muhammad <muhammad.ksht@gmail.com>
2021-09-14 14:19:43 +0430

    added basic lottie

--- src/components/header/roshan-earth-background/roshan-earth-background.js
+++ src/components/header/roshan-earth-background/roshan-earth-background.js
@@ -6,11 +6,27 @@ import roshanBigTitle from "../../../assets/images/roshan-big-title.svg";
 import description from "../../../assets/images/description.svg";
 import descriptionMobile from "../../../assets/images/description-mobile.svg";
 import backgroundMobile from "../../../assets/images/background-mobile.png";
+import Lottie from 'lottie-react';
+import mouseAnimation from '../../../assets/lotties/mouse-scrolling.json';
+
 
 class RoshanEarthBackground extends Component {
     render() {
+        const mouseAnimationStyle = {
+            height: 500,
+            marginTop:600,
+            marginRight:530,
+            display:'flex',
+            position:'absolute',
+        };
+
         return(
             <div className='earth-background'>
+                <Lottie
+                    animationData={mouseAnimation}
+                    style={mouseAnimationStyle}
+                    className={'mouse-animation'}
+                />
                 <img className='background' src={background} alt={'fail'}/>
                 <img className='background-mobile' src={backgroundMobile} alt={'fail'}/>
                 <img className='big-title' src={roshanBigTitle} alt={'fail'}/>

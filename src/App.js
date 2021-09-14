237ef933aabe9aaf5de7a6b983e858eec6b02fa0
Muhammad <muhammad.ksht@gmail.com>
2021-09-14 19:36:13 +0430

    added scroll

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import {Component} from 'react';
 import Header from "./components/header/header-wrapper";
 import './App.css'
-
+import ScrollableContainer from "react-full-page-scroll";
 
 class RoshanWebsite extends Component {
 
   render() {
 
     return(
-        <div className='roshan-website'>
-            <Header/>
-        </div>
+
+      <ScrollableContainer animationTime={2000}>
+          <div className='roshan-website'>
+              <Header/>
+          </div>
+          <div className='roshan-website'>
+              <Header/>
+          </div>
+      </ScrollableContainer>
     )
   }
 }

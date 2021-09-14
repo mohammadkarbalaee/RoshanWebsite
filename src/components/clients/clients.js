b93e13d4a9df2590bb994c02e89949f65356861c
Muhammad <muhammad.ksht@gmail.com>
2021-09-14 22:52:26 +0430

    added mobile footer

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {ReactComponent as ClientsMobile} from "../../assets/images/clients.svg";
+import {ReactComponent as FooterMobile} from "../../assets/images/mobile-footer.svg";
 import './clients.scss'
 
 class Clients extends Component{
@@ -7,6 +8,12 @@ class Clients extends Component{
         return(
             <div id={'clients'}>
                 <ClientsMobile/>
+                <div id={'space'}/>
+                <div className={'footer'}>
+                    <FooterMobile/>
+                    <div id={'space-row'}/>
+                </div>
+                <div id={'space-bottom'}/>
             </div>
         )
     }

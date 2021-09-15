ed8393f35d1f5f7bbbae72b189c50d21ae9c0e59
Muhammad <muhammad.ksht@gmail.com>
2021-09-15 10:08:36 +0430

    fixed foote

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
-import {ReactComponent as ClientsMobile} from "../../assets/images/clients.svg";
-import {ReactComponent as FooterMobile} from "../../assets/images/mobile-footer.svg";
+import {ReactComponent as MoshtarianMobile} from "../../assets/images/moshtarian.svg";
 import './clients.scss'
 
 class Clients extends Component{
     render() {
         return(
             <div id={'clients'}>
-                <ClientsMobile/>
-                <div id={'space'}/>
-                <div className={'footer'}>
-                    <FooterMobile/>
-                    <div id={'space-row'}/>
-                </div>
-                <div id={'space-bottom'}/>
+                <MoshtarianMobile/>
             </div>
         )
     }

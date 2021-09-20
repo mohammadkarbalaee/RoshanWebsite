536119c4376cc7b45f7008c8f598c019c92ecf1e
Muhammad <muhammad.ksht@gmail.com>
2021-09-20 20:00:19 +0430

    deleted unwanted codes

--- src/components/header/header-wrapper.js
+++ src/components/header/header-wrapper.js
@@ -5,31 +5,11 @@ import Navbar from "./navigation-bar/navbar";
 import RoshanEarthBackground from "./roshan-earth-background/roshan-earth-background";
 import HamkariBamaButton from "./navigation-bar/left-text-group/hamkari-bama-button/hamkari-bama-button";
 import TamasBamaButton from "./navigation-bar/left-text-group/tamas-bama-button/tamas-bama-button";
-import HamakariBamaPage from "./navigation-bar/hamkari-bama-page/hamkari-bama-page";
-import TamasBamaPage from "./navigation-bar/tamas-bama-page/tamas-bama-page"
 
 class Header extends Component {
     state = {
         isOpen:false,
-        isHamkariClickedMobile: false,
-        isTamasBamaClickedMobile: false,
-        isHamkariClickedWeb: false,
-        isTamasBamaClickedWeb: false,
-        isFirstRender:true,
-
     }
-
-    onTamasClickWeb = () => {
-        this.setState({
-           isTamasBamaClickedWeb: !this.state.isTamasBamaClickedWeb
-        });
-    };
-
-    onHamkariClickWeb = () => {
-        this.setState({
-           isHamkariClickedWeb: !this.state.isHamkariClickedWeb
-        });
-    };
 
     toggle = () => {
         this.setState({
@@ -37,74 +17,21 @@ class Header extends Component {
         })
     };
 
-    toggleHamkariBama = () => {
-        this.toggle();
-
-        this.setState({
-            isHamkariClickedMobile: !this.state.isHamkariClickedMobile
-        })
-    };
-
-    toggleTamasBama = () => {
-        this.toggle();
-
-        this.setState({
-            isTamasBamaClickedMobile: !this.state.isTamasBamaClickedMobile
-        })
-    };
-
     reactToToggles = () => {
-        if(this.state.isHamkariClickedWeb || this.state.isTamasBamaClickedWeb){
-            if(this.state.isHamkariClickedWeb){
-                this.state.isHamkariClickedWeb = !this.state.isHamkariClickedWeb;
-
-                return (
-                    <HamakariBamaPage/>
-                )
-            } else {
-                this.state.isTamasBamaClickedWeb = !this.state.isTamasBamaClickedWeb;
-                return(
-                    <TamasBamaPage/>
-                )
-            }
+        if (this.state.isOpen) {
+            return (
+                <div id={'compact-menu'}>
+                    <TamasBamaButton onClick={this.toggleTamasBama}/>
+                    <HamkariBamaButton onClick={this.toggleHamkariBama}/>
+                </div>
+            )
         } else {
-            if(this.state.isFirstRender){
-
-                if(this.state.isTamasBamaClickedMobile){
-                    this.state.isFirstRender = !this.state.isFirstRender;
-                    this.state.isTamasBamaClickedMobile = !this.state.isTamasBamaClickedMobile;
-                    return(
-                        <TamasBamaPage/>
-                    )
-                } else if(this.state.isHamkariClickedMobile) {
-                    this.state.isFirstRender = !this.state.isFirstRender;
-                    this.state.isHamkariClickedMobile = !this.state.isHamkariClickedMobile;
-                    return(
-                        <HamakariBamaPage/>
-                    )
-                } else if (this.state.isOpen) {
-                    return(
-                        <div id={'compact-menu'}>
-                            <TamasBamaButton onClick={this.toggleTamasBama}/>
-                            <HamkariBamaButton onClick={this.toggleHamkariBama}/>
-                        </div>
-                    )
-                } else {
-                    return(
-                        <RoshanEarthBackground/>
-                    )
-                }
-            } else {
-                this.state.isFirstRender = !this.state.isFirstRender;
-                return(
-                    <div id={'compact-menu'}>
-                        <TamasBamaButton onClick={this.toggleTamasBama}/>
-                        <HamkariBamaButton onClick={this.toggleHamkariBama}/>
-                    </div>
-                )
-            }
+            return (
+                <RoshanEarthBackground/>
+            )
         }
     }
+
 
     render() {
         return(
@@ -113,8 +40,6 @@ class Header extends Component {
                     <Navbar
                         toggle={this.toggle}
                         isOpen={this.state.isOpen}
-                        onHamkariClick={this.onHamkariClickWeb}
-                        onTamasBamaClick={this.onTamasClickWeb}
                     />
                     {this.reactToToggles()}
                 </div>

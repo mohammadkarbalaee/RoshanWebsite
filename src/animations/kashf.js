5625a628b4d6f18d7475075bec20c87154c45ac6
Muhammad <muhammad.ksht@gmail.com>
2021-09-22 16:14:37 +0330

    added kashf animations

--- src/animations/kashf.js
+++ src/animations/kashf.js
@@ -5,7 +5,75 @@ export const flowerAndFrameAnimation = {
     visible: {
         opacity: 1,
         transition: {
+            delay:1,
+            duration: 0.8
+        }
+    },
+};
+
+export const dotAnimation = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            delay:0.1,
+            duration: 0.5
+        }
+    },
+};
+
+export const mainLineAnimation = {
+    hidden: {
+        pathLength:0,
+
+    },
+    visible: {
+        pathLength:1,
+        transition: {
+            duration: 1.5
+        }
+    },
+};
+
+export const subLineAnimation = {
+    hidden: {
+        pathLength:0
+    },
+    visible: {
+        pathLength:1,
+        transition: {
+            delay:0.5,
             duration: 1
+        }
+    },
+};
+
+export const polygonAnimation = {
+    hidden: {
+        pathLength:0
+    },
+    visible: {
+        pathLength:1,
+        transition: {
+            delay:1,
+            duration: 0.8
+        }
+    },
+};
+
+export const textAnimation = {
+    hidden: {
+        opacity: 0,
+        x:20,
+    },
+    visible: {
+        opacity: 1,
+        x:0,
+        transition: {
+            delay:1,
+            duration: 0.5
         }
     },
 };

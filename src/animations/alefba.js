0d6f683f3e87c2391456bf8d3555243f3f180339
Muhammad <muhammad.ksht@gmail.com>
2021-09-22 20:27:00 +0330

    finished alefba mobile

---
+++ src/animations/alefba.js
@@ -0,0 +1,51 @@
+export const backgroundAnimationMobile = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 0.3,
+        transition: {
+            delay:2,
+            duration: 0.8
+        }
+    },
+};
+
+export const aboutAnimationMobile = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            delay:2,
+            duration: 0.6
+        }
+    },
+};
+
+export const bottomLineAnimationMobile = {
+    hidden: {
+        pathLength:0
+    },
+    visible: {
+        pathLength:1,
+        transition: {
+            delay:1.1,
+            duration: 0.8
+        }
+    },
+};
+
+export const boxAnimationMobile = {
+    hidden: {
+        pathLength:0
+    },
+    visible: {
+        pathLength:1,
+        transition: {
+            delay:1.7,
+            duration: 1.1
+        }
+    },
+};

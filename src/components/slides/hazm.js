7993b34a44ebc32c5493c2395153b7c21a12115a
Muhammad <muhammad.ksht@gmail.com>
2021-09-23 21:14:28 +0330

    slides animation is done

--- src/components/slides/hazm.js
+++ src/components/slides/hazm.js
@@ -1,6 +1,6 @@
 import HazmMobile from "../jsx-svgs/mobile/hazm";
 import './slides-style.scss'
-import {ReactComponent as HazmWebImage} from "../../assets/images/web-hazm-image.svg";
+import HazmWebImage from "../jsx-svgs/web/hazm-image";
 import HazmWebText from "../jsx-svgs/web/hazm-text";
 
 function Hazm() {
@@ -13,7 +13,9 @@ function Hazm() {
                 <div id={'web'}>
                     <HazmWebText id={'text'}/>
                     <div id={'space-between'}/>
-                    <HazmWebImage id={'image'}/>
+                    <div id={'image'}>
+                        <HazmWebImage/>
+                    </div>
                 </div>
             </div>
         </div>

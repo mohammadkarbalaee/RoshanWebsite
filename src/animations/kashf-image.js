8597d7080a4f0de3101a4db6fc490cd7aa0df23c
Muhammad <muhammad.ksht@gmail.com>
2021-09-23 18:55:38 +0330

    kashf mobile animation is implemented

---
+++ src/animations/kashf-image.js
@@ -0,0 +1,12 @@
+export const flowerAnimation = {
+    hidden: {
+        opacity: 0.2
+    },
+    visible: {
+        rotate:360,
+        opacity: 1,
+        transition: {
+            duration: 5
+        }
+    },
+};

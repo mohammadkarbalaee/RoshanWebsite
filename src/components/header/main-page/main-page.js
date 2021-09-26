64c7e2c4e9ddb5c7777e74015e740da4b9ef08a2
Muhammad <muhammad.ksht@gmail.com>
2021-09-26 08:37:48 +0330

    web background is fixed

--- src/components/header/main-page/main-page.js
+++ src/components/header/main-page/main-page.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import './main-page.scss'
-import WebBackground from "../../../assets/images/web-background.svg";
-import MobileBackground from "../../../assets/images/mobile-background.svg";
-import {motion} from 'framer-motion';
-import {startingAnimation,scrollDownAnimation} from "../../../animations/main-page";
-
-const animationChooser = (scrollQuantity) => {
-
-    if(scrollQuantity === 1){
-        return scrollDownAnimation;
-    } else {
-        return startingAnimation;
-    }
-};
+import WebBackground from "../../jsx-svgs/web/background";
+import MobileBackground from "../../jsx-svgs/mobile/background";
 
 function MainPage(props) {
     return (
         <div className='all'>
-            <motion.img
-                src={WebBackground}
-                alt={'failed'}
-                className='web-background'
-                initial={animationChooser(props.scrollQuantity).initial}
-                animate={animationChooser(props.scrollQuantity).animate}
-                transition={animationChooser(props.scrollQuantity).transition}
-            />
-            <motion.img
-                src={MobileBackground}
-                 alt={'failed'}
-                 className='mobile-background'
-                initial={animationChooser(props.scrollQuantity).initial}
-                animate={animationChooser(props.scrollQuantity).animate}
-                transition={animationChooser(props.scrollQuantity).transition}
-            />
+            <WebBackground className='web-background'/>
+            <MobileBackground className='mobile-background'/>
         </div>
     )
 }

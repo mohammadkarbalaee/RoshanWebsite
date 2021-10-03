e19870be5c1ec1187428351beb16c3ae67c74c70
Muhammad <muhammad.ksht@gmail.com>
2021-10-03 14:53:37 +0330

    well-scaled the navbar

--- src/components/header/navigation-bar/links-group/contact-us-button/contact-us-button.js
+++ src/components/header/navigation-bar/links-group/contact-us-button/contact-us-button.js
@@ -1,11 +1,12 @@
 import './contact-us-button.scss';
 import {Link} from 'react-router-dom';
+import {ReactComponent as Text} from "../../../../../assets/images/contact-us-text.svg";
 
 function ContactUsButton() {
     return (
         <div className='contact-us'>
-            <Link to={'/contact-us'} id={'button'}>
-                تماس با ما
+            <Link to={'/contact-us'}>
+                <Text id={'button'}/>
             </Link>
         </div>
     )
--- src/components/header/navigation-bar/links-group/join-us-button/join-us-button.js
+++ src/components/header/navigation-bar/links-group/join-us-button/join-us-button.js
@@ -1,11 +1,12 @@
 import './join-us-button.scss'
 import {Link} from 'react-router-dom';
+import {ReactComponent as Text} from "../../../../../assets/images/join-us-text.svg";
 
 function JoinUsButton() {
     return (
         <div className="join-us">
-            <Link to={'/join-us'} id={'button'}>
-                همکاری با ما
+            <Link to={'/join-us'}>
+                <Text id={'button'}/>
             </Link>
         </div>
     )

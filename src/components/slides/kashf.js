cc6d56555701d6af1e01cbe7c1e03b21bb7460d4
Muhammad <muhammad.ksht@gmail.com>
2021-10-03 15:35:04 +0330

    scaled kashf web

--- src/components/slides/kashf.js
+++ src/components/slides/kashf.js
@@ -1,8 +1,8 @@
 import KashfMobile from "../jsx-svgs/mobile/kashf";
 import KashfWebText from "../jsx-svgs/web/kashf-text";
 import KashfWebImage from "../jsx-svgs/web/kashf-image";
-import './slides-style.scss'
-import {isFirefox, isMobile} from "react-device-detect";
+import './kashf.scss'
+import {isMobile} from "react-device-detect";
 
 const returnBasedOneDevice = (props) => {
     if(isMobile){
@@ -11,9 +11,9 @@ const returnBasedOneDevice = (props) => {
                 <KashfMobile status={props.scrollQuantity}/>
             </div>
         )
-    } else if(isFirefox){
+    }  else {
         return(
-            <div id={'web-firefox'}>
+            <div id={'web'}>
                 <div id={'image'}>
                     <KashfWebImage status={props.scrollQuantity}/>
                 </div>
@@ -21,16 +21,6 @@ const returnBasedOneDevice = (props) => {
                 <div id={'text'}>
                     <KashfWebText status={props.scrollQuantity}/>
                 </div>
-            </div>
-        )
-    } else {
-        return(
-            <div id={'web'}>
-                <div id={'image'}>
-                    <KashfWebImage status={props.scrollQuantity}/>
-                </div>
-                <div id={'space-between'}/>
-                <KashfWebText id={'text'} status={props.scrollQuantity}/>
             </div>
         )
     }
